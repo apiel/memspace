f2a634e1161f46e6a8b18e3dc250baabba50848a
Alex <alexandre.piel@gmail.com>
2015-12-20 20:35:50 +0100

    write data

--- controllers/api/data.js
+++ controllers/api/data.js
@@ -10,12 +10,19 @@ var data = express.Router();
 });*/
 
 data.get('/:key', function(req, res) {
-    var key = req.param('key');
+    var key = req.params.key;
     
     memspace.memspace.setName(req.query.memspace);
-    var user = memspace.user.loadBySession(req.query.sessionId);    
-    var content = memspace.data.read(key, user);
-    res.send(content);
+    var user = memspace.user.loadBySession(req.query.sessionId); 
+    var response = null;
+    if (typeof(req.query.write) !== 'undefined') {
+        memspace.data.read(key, user, req.query.write);
+        response = JSON.stringify({'success': true});
+    }
+    else {
+        response = memspace.data.read(key, user);
+    }
+    res.send(response);
 });
 
 module.exports = data;
